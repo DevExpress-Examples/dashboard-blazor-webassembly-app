ad41b6ae8a0ab162d73a4040afbbbe29d07dd874
Polina Tyureva <polina.tyureva@devexpress.com>
2022-06-03 11:02:58 +0400

    update solution to .net 6

--- CS/BlazorDashboardApp/Server/Controllers/DefaultDashboardController.cs
+++ CS/BlazorDashboardApp/Server/Controllers/DefaultDashboardController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.DataProtection;
 
 namespace BlazorDashboardApp.Server {
     public class DefaultDashboardController : DashboardController {
-        public DefaultDashboardController(DashboardConfigurator configurator, IDataProtectionProvider dataProtectionProvider = null)
+        public DefaultDashboardController(DashboardConfigurator configurator, IDataProtectionProvider? dataProtectionProvider = null)
             : base(configurator, dataProtectionProvider) {
         }
     }
